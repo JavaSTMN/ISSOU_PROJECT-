ae12c5c232eb6dea6a178dee7173951f71a1ebf6
loris demaria <loris.d@hotmail.fr>
2018-11-19 16:37:07 +0100

    gogolien

---
+++ Picross_Issou/src/LabelPicross.java
@@ -0,0 +1,78 @@
+import java.util.ArrayList;
+
+import javax.swing.BoxLayout;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
+public class LabelPicross {
+	
+	public JPanel panelList;
+	public boolean orientation;
+	public ArrayList<ArrayList<Integer>> listCond;
+	public int sizeCond; 
+	public JPanel panel;
+	
+	public LabelPicross(boolean orientation, ArrayList<ArrayList<Integer>> listCond) {
+		
+	this.orientation=orientation;
+	this.listCond=listCond;
+	this.panelList = new JPanel();
+	this.sizeCond = listCond.size();
+	
+	panel = CreatePanel(this.orientation, this.listCond);
+	
+		
+	}
+	
+	private JPanel CreatePanel(boolean orientation, ArrayList<ArrayList<Integer>> listCond) {
+		
+		if(orientation==true) // Horizontal
+		{
+		this.panelList.setLayout(new BoxLayout(this.panelList, BoxLayout.X_AXIS));
+		for(ArrayList<Integer> numero : listCond)
+		{
+			JPanel panel = new JPanel();
+			BoxLayout boxLayout = new BoxLayout(panel, BoxLayout.Y_AXIS); // top to bottom 	
+		    panel.setLayout(boxLayout);
+		    for(int nombre : numero)
+			{
+				String str = Integer.toString(nombre);
+				panel.add(new JLabel(str+ "  "));
+			}
+			panelList.add(panel);
+		}
+		
+		return panelList;	
+		}
+		else
+		{
+		this.panelList.setLayout(new BoxLayout(this.panelList, BoxLayout.Y_AXIS));
+		for(ArrayList<Integer> numero : listCond)
+			{
+			JPanel panel = new JPanel();
+			BoxLayout boxLayout = new BoxLayout(panel, BoxLayout.X_AXIS); // top to bottom 	
+		    panel.setLayout(boxLayout);
+		    for(int nombre : numero)
+				{
+				String str = Integer.toString(nombre);
+				panel.add(new JLabel(str+ " "));
+				}
+				panelList.add(panel);
+			}	
+		}
+		return panelList;	
+	}
+		
+
+	
+	
+	
+	public JPanel getPanel() {
+		return this.panel;
+	}
+			
+	}
+
+	
+
