8ca056b8164be058b2d12b1e7c2b851b879a10ef
Quentin Luppo <quentinluppo@orange.fr>
2018-11-26 21:49:48 +0100

    try to work

--- Picross_Issou/src/LayoutFactory.java
+++ Picross_Issou/src/LayoutFactory.java
@@ -2,13 +2,11 @@ import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
-
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Iterator;
-
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 
